e9272e69647bd5e8aaf255c1775c81acc920dc8e
vishalmoharikar <vishalmoharikar@gmail.com>
2026-05-13 10:52:58 +0530

    added api-service

---
+++ api-gateway/src/main/java/com/taskplatform/gateway/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,64 @@
+package com.taskplatform.gateway.filter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+
+@Component
+public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
+    private static final String JWT_SECRET = "my-super-secret-jwt-key-for-learning-only";
+    private final SecretKey key = Keys.hmacShaKeyFor(JWT_SECRET.getBytes(StandardCharsets.UTF_8));
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        String path = request.getURI().getPath();
+        if (isPublic(path)) {
+            return chain.filter(exchange);
+        }
+
+        String authHeader = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        if(authHeader==null || !authHeader.startsWith("Bearer")) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        String token = authHeader.substring(7);
+        try{
+            Claims claim = Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+            ServerHttpRequest modified = request.mutate()
+                    .header("X-User-Id",claim.getSubject())
+                    .build();
+            return chain.filter(exchange.mutate().request(modified).build());
+        }catch (Exception ex){
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+    }
+
+    private boolean isPublic(String path) {
+        return path.contains("/users/register") || path.contains("/users/login");
+    }
+
+    @Override
+    public int getOrder() {
+        return -1; //Runs before any other filter
+    }
+}
